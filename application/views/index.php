bd4c55aad7e6a16d9b60bb62741622537b5b9a9a
Milton Otavo <milton.otavo@gmail.com>
2017-07-29 15:47:11 -0500

    css

--- application/views/index.php
+++ application/views/index.php
@@ -60,19 +60,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			
 			
 		</body>
-		<!--   Core JS Files   -->
-		<script src="<?= base_url(); ?>public/js/jquery.min.js" type="text/javascript"></script>
-		<script src="<?= base_url(); ?>public/js/bootstrap.min.js" type="text/javascript"></script>
-		<script src="<?= base_url(); ?>public/js/material.min.js"></script>
-
-		<!--  Plugin for the Datepicker, full documentation here: http://www.eyecon.ro/bootstrap-datepicker/ -->
-		<script src="<?= base_url(); ?>public/js/bootstrap-datepicker.js" type="text/javascript"></script>
-		<!-- Control Center for Material Kit: activating the ripples, parallax effects, scripts from the example pages etc -->
-		<script src="<?= base_url(); ?>public/js/material-kit.js" type="text/javascript"></script>
-		<script src="<?= base_url(); ?>public/js/moment.js"></script> 	
-		<script src="<?= base_url(); ?>public/js/moment-with-locales.min.js"></script> 	
-
-
-		
+		<?php $this->load->view('overall/footer'); ?>
 
 		</html>
