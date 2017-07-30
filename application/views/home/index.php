7caa64bb3b3d9046ac129a381849eb8e5830364f
Milton Otavo <milton.otavo@gmail.com>
2017-07-29 23:03:54 -0500

    Home

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -18,7 +18,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							<!-- tables row -->
 							<div class="table-responsive">
 								<table class="table" id="bets">
-									
+
 									<tbody>
 										
 										
@@ -123,42 +123,42 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 						
 						var compe = item.ID;
-						
+
 						$.getJSON('<?= base_url(); ?>ajax/json_match', {fecha: fecha, compe:compe}, function(match) {
 							$.each(match, function(a, row) {
 								$("#bets > tbody #"+compe+"").after('\
 									<tr id='+ row.ID+'>\
-										<td class="" >'+ row.HORARIO+'</td>\
-										<td class="" >'+ row.LOCAL+'</td>\
-										<td class="" >'+ row.VISITANTE+'</td>\
-										<td class="" >'+ isEmpty(row._1)+'</td>\
-										<td class="" >'+ isEmpty(row._X)+'</td>\
-										<td class="" >'+ isEmpty(row._2)+'</td>\
-										<td class="" >'+ isEmpty(row._1X)+'</td>\
-										<td class="" >'+ isEmpty(row._12)+'</td>\
-										<td class="" >'+ isEmpty(row._2X)+'</td>\
-										<td class="" >'+ isEmpty(row.UNDER_25)+'</td>\
-										<td class="" >'+ isEmpty(row.OVER_25)+'</td>\
-										<td class="" >'+ isEmpty(row.GG)+'</td>\
-										<td class="" >'+ isEmpty(row.NG)+'</td>\
+										<td  class="text-center" width="6%" >'+ row.HORARIO+'</td>\
+										<td  class="text-left" width="12%" >'+ row.LOCAL+'</td>\
+										<td  class="text-left" width="12%" >'+ row.VISITANTE+'</td>\
+										<td  class="text-center" width="7%" >'+ isEmpty(row._1)+'</td>\
+										<td  class="text-center" width="7%" >'+ isEmpty(row._X)+'</td>\
+										<td  class="text-center" width="7%" >'+ isEmpty(row._2)+'</td>\
+										<td  class="text-center" width="7%" >'+ isEmpty(row._1X)+'</td>\
+										<td  class="text-center" width="7%" >'+ isEmpty(row._12)+'</td>\
+										<td  class="text-center" width="7%" >'+ isEmpty(row._2X)+'</td>\
+										<td  class="text-center" width="7%" >'+ isEmpty(row.UNDER_25)+'</td>\
+										<td  class="text-center" width="7%" >'+ isEmpty(row.OVER_25)+'</td>\
+										<td  class="text-center" width="7%" >'+ isEmpty(row.GG)+'</td>\
+										<td  class="text-center" width="7%" >'+ isEmpty(row.NG)+'</td>\
 									</tr>\
 									');
 							});
 							$("#bets > tbody #"+compe+"").after('\
 								<tr>\
-									<th>Hora</th>\
-									<th>Local</th>\
-									<th>Visitante</th>\
-									<th>1</th>\
-									<th>X</th>\
-									<th>2</th>\
-									<th>1X</th>\
-									<th>12</th>\
-									<th>2X</th>\
-									<th>OVER</th>\
-									<th>UNDER</th>\
-									<th>GG</th>\
-									<th>NG</th>\
+									<th class="text-center">Hora</th>\
+									<th class="text-left">Local</th>\
+									<th class="text-left">Visitante</th>\
+									<th class="text-center">1</th>\
+									<th class="text-center">X</th>\
+									<th class="text-center">2</th>\
+									<th class="text-center">1X</th>\
+									<th class="text-center">12</th>\
+									<th class="text-center">2X</th>\
+									<th class="text-center">OVER</th>\
+									<th class="text-center">UNDER</th>\
+									<th class="text-center">GG</th>\
+									<th class="text-center">NG</th>\
 								</tr>\
 								');
 
