6c771b20340185695b94ab5908df106bf25dd2f8
Milton Otavo <milton.otavo@gmail.com>
2017-08-09 11:26:02 -0500

    Ajuste Horario

--- application/controllers/syncBetfair.php
+++ application/controllers/syncBetfair.php
@@ -55,49 +55,67 @@ class syncBetfair extends CI_Controller {
 	public function syncEventos(){
 		$from= date("Y-m-d");
 		$to= "2017-08-10";
+		$i=0;
 		$competencias=$this->ApiBetfair->getCompetencias(); 
 		if ($competencias!=null) {
 			foreach ($competencias as $key => $rowCompe) {
 				$eventos=$this->ApiBetfair->getEventos($from, $to, $rowCompe['competition']['id']); 
 				if ($eventos!=null) {
-					print_r($eventos);
 					foreach ($eventos as $key => $rowEvent) {
-						
 						$openDate=explode("T", $rowEvent['event']['openDate']);
 						$fecha=$openDate[0];
 						$hora= substr($openDate[1], 0, 5 );
-
+						#print_r($rowEvent);
+						#Funcion Horario Colombia
+						switch ($rowEvent['event']['timezone']) {
+							case 'Europe/London':
+							# Resta 5 horas UTC
+							$fecha=strtotime($fecha.' '.$hora);
+							$fecha = date("Y-m-d H:i", $fecha);
+							$fecha = new DateTime($fecha);
+							$fecha->sub(new DateInterval('PT5H'));
+							break;
+						}
+ 
 						$veri=strpos($rowEvent['event']['name'], " v ");
-						if ($veri===false) {
+						if ($veri=="") {
 							$name=explode(" - ", $rowEvent['event']['name']);
 						} else {
 							$name=explode(" v ", $rowEvent['event']['name']);
 						}
-						$local= $name[0];
-						$visitante= $name[1];
-					 
+						if (isset($name[0]) and isset($name[1]) ) {
+							$local= $name[0];
+							$visitante= $name[1];
+						}
+
+
 						$this->Partido->ID_PARTIDO     = $rowEvent['event']['id'];
 						$this->Partido->ID_COMPETENCIA = $rowCompe['competition']['id'];
-						$this->Partido->ID_PAIS        = $rowCompe['event']['countryCode'];
-						$this->Partido->TIMEZONE       = $rowCompe['event']['id'];
-						$this->Partido->FECHA          = $fecha;
-						$this->Partido->HORA           = $hora;
+						if (isset($rowEvent['event']['countryCode'])) {
+							$this->Partido->ID_PAIS        = $rowEvent['event']['countryCode'];
+						}
+						
+						$this->Partido->TIMEZONE       = $rowEvent['event']['timezone'];
+						$this->Partido->FECHA          = $fecha->format('Y-m-d');
+						$this->Partido->HORA           = $fecha->format('H:i');
 						$this->Partido->LOCAL          = $local;
 						$this->Partido->VISITANTE      = $visitante;
-						$this->Partido->MARKET_COUNT   = $rowCompe['marketCount'];
+						$this->Partido->MARKET_COUNT   = $rowEvent['marketCount'];
 
 						$Partido=$this->Partido->getPartido();
 						if ($Partido==null) {
-							echo "<b>add</b> ".$rowEvent['event']['name']. " " .$rowEvent['event']['openDate']. "<br>";
+							echo "<b>add</b> ".$rowEvent['event']['name']. " " .$fecha->format('Y-m-d H:i'). "<b$fecha->format('Y-m-d H:i')r>";
 							$this->Partido->add();
 						} else {
-							echo "<b>update</b> ".$rowEvent['event']['name']." ". $rowEvent['event']['openDate']. "<br>";
+							echo "<b>update</b> ".$rowEvent['event']['name']." ". $fecha->format('Y-m-d H:i'). "<br>";
 							$this->Partido->update();
 						}
+						$i++;
 					}
 				}
 
 			}
+			echo $i." Partidos";
 		} else {
 			echo "No hay competencias";
 		}
