fa0bcc4dff8c2b1b41ef09bfb1de1082d53c4ff3
Milton Otavo <milton.otavo@gmail.com>
2017-08-20 14:41:19 -0500

    Dashboard

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ date_default_timezone_set('America/Bogota');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/bet/';
+$config['base_url'] = 'http://localhost:8080/prestamos/';
 
 /*
 |--------------------------------------------------------------------------
